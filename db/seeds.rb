73b881fb4258ff464a1be93d0802c104b6e21843
Anis4300 <bouhouche.anis@gmail.com>
2019-05-08 09:13:07 +0200

    enfin

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,22 @@ puts " ✔"
 
 10.times do
   user = User.create!(
-                      first_name: Faker::Name.first_name, 
-                      last_name: Faker::Name.last_name,
-                      email: Faker::Name.first_name + "@yopmail.com",
-                      description: Faker::Quote.famous_last_words,
-                      password: "password"
-                      )
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name,
+     email: Faker::Name.first_name + "@yopmail.com",
+    description: Faker::Quote.famous_last_words,
+    password: "password"
+  )
 end
 
 10.times do
   user = Event.create!(
-                      start_date: Faker::Date.between(1.days.from_now, 15.days.from_now),
-                      duration: 60,
-                      title: Faker::Lorem.sentence(2),
-                      description: Faker::Lorem.sentence(15),
-                      price: 100,
-                      location: "Grenoble",
-                      administrator: User.all.sample
-                      )
+    start_date: Faker::Date.between(1.days.from_now, 15.days.from_now),
+    duration: 60,
+    title: Faker::Lorem.sentence(2),
+    description: Faker::Lorem.sentence(15),
+    price: 100,
+    location: Faker::Address.city,
+    administrator: User.all.sample
+  )
 end
